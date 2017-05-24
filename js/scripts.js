92582681f19ae783de0df392eb02a03b35984515
Mateusz <matstepien@outlook.com>
2017-05-24 17:10:31 +0200

    new comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 // scripts.js
-
 var name = prompt('Enter your name');
+// alert is here
 alert('Hello, ' + name);
+/*
+  block comment
+  console.log ('Hello, ' + name); The commented will not be called
+*/
 console.log('Hello, ' + name);
